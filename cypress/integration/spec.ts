fd39e5957cc9bd6836e1c4df62bb801cd1ff705e
Chris Stone <contact@chrisstone.dev>
2022-01-17 14:21:52 -0500

    Expect readable page title

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,5 +2,6 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
     cy.contains('Search for GitHub user');
+    cy.title().should('equal', 'GitHub User Search');
   });
 });
